e0a1ed4540d7331f50d1eeb372f666c39e70c8b5
Killman03 <122359310+Killman03@users.noreply.github.com>
2025-03-24 09:14:33 +0600

    ver 1.2 24.03.25 | Change db, fix catalog.html

--- promarket/promarket/settings.py
+++ promarket/promarket/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'promarket.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'promarket',
+        'USER': 'postgres',
+        'PASSWORD': '1234567890',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -127,4 +131,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
